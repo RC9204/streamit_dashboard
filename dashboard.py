1d30a550875b8532cd8c037460bd0c9705e8a12f
RC9204 <116723803+RC9204@users.noreply.github.com>
2024-07-12 16:25:56 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,19 +1,29 @@
 import streamlit as st
-import requests
 import pandas as pd
+import requests
 import matplotlib.pyplot as plt
 import shap
 
+
 api_url = "https://projet-17-rcuaz-heroku.herokuapp.com/predict"
 
+
 df_modif_allege = pd.read_csv('df_modif_allege.csv')
+
+
 selected_client_id = st.selectbox('Selectionnez un client :', df_modif_allege['SK_ID_CURR'])
 
 if st.button('Obtenir les prédictions'):
+  
     client_data = df_modif_allege[df_modif_allege['SK_ID_CURR'] == selected_client_id]
     data = client_data.to_dict(orient='records')[0]
 
+
     response = requests.post(api_url, json=data)
+    
+
+    st.write(f"Status Code: {response.status_code}")
+    st.write(f"Response Text: {response.text}")
     
     if response.status_code == 200:
         result = response.json()
