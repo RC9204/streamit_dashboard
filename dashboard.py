d58ec2703f7ec3a7f344bbdaf2ce7221fea15f0f
RC9204 <116723803+RC9204@users.noreply.github.com>
2024-07-12 16:16:15 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,33 +4,20 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import shap
 
-api_url = 'https://projet-17-rcuaz-heroku.herokuapp.com/predict'
+api_url = "https://projet-17-rcuaz-heroku.herokuapp.com/predict"
 
 df_modif_allege = pd.read_csv('df_modif_allege.csv')
+selected_client_id = st.selectbox('Selectionnez un client :', df_modif_allege['SK_ID_CURR'])
 
-train, test = train_test_split(df_modif_allege, test_size=0.2, random_state=0)
-train.drop(columns=['index'], inplace=True)
-test.drop(columns=['index'], inplace=True)
-X_train = train.drop(columns=['TARGET'])
-y_train = train['TARGET']
-X_test = test.drop(columns=['TARGET'])
-y_true = test['TARGET']
+if st.button('Obtenir les prédictions'):
+    client_data = df_modif_allege[df_modif_allege['SK_ID_CURR'] == selected_client_id]
+    data = client_data.to_dict(orient='records')[0]
 
-selected_client_id = st.selectbox('Selectionnez un client :', X_test['SK_ID_CURR'])
-random_observation_df = X_test[X_test['SK_ID_CURR'] == selected_client_id]
-
-data = random_observation_df.to_dict(orient='records')[0]
-
-response = requests.post(api_url, json=data)
-prediction = response.json()['prediction'][0]
-
-client_id = random_observation_df['SK_ID_CURR'].values[0]
-
-st.title(f"Dashboard de Credit")
-st.markdown(f"### Num client: {client_id}")
-
-credit_accepted = 'Non' if prediction == 1 else 'Oui'
-st.markdown(f"### Credit accepte: {credit_accepted}")
-
-st.markdown(f"### Score du client")
-st.markdown(f"Score: {prediction:.2f}")
+    response = requests.post(api_url, json=data)
+    
+    if response.status_code == 200:
+        result = response.json()
+        st.write(f"Prédiction : {result['prediction']}")
+        st.write(f"Probabilité : {result['probability']}")
+    else:
+        st.write("Erreur lors de l'obtention des prédictions.")
